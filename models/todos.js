aa2f2f8a4a48411f3b511b654db9d12cc266c58c
Juan Granja <ggjuanb@hotmail.com>
2020-10-03 17:41:51 -0500

    first commit

---
+++ models/todos.js
@@ -0,0 +1,20 @@
+const { model } = require('mongoose');
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+
+const TodoSchema = new Schema({
+    action: {
+        type: String,
+        required: [true, 'The todo text field is required']
+    }
+})
+
+
+const Todo = mongoose.model('todo',TodoSchema);
+
+
+
+model.exports = Todo;
+
